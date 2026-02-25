63f3668c248626bb09b3e95e20f3d2d0a8299ea5
ruxshona <ruxshonaaglamkhujayeva@gmail.com>
2026-02-25 16:27:14 +0500

    erpnext_with_ibox/ibox/config.py

---
+++ erpnext_with_ibox/ibox/config.py
@@ -0,0 +1,11 @@
+# IBOX API Endpoints
+IBOX_ENDPOINTS = {
+	"items": "/api/integration/core/directory",
+	# ...other endpoints
+}
+
+# Directory Types
+DIRECTORY_TYPES = {
+	"items": "product_selection",
+	# ...other types
+}
